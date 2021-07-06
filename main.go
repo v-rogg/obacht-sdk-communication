5359c597cd1403f4505d1b48b344f9334567783b
v-rogg <mail@valentinrogg.de>
2021-07-06 16:43:59 +0200

    Add CORS Refactor Add websocket relay

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/websocket/v2"
 	"log"
 )
+
+var mqttClient mqtt.Client
+var cubePosition [2]int64
+
+func init() {
+	cubePosition[0] = 0
+	cubePosition[1] = 0
+}
 
 func main() {
 
@@ -12,7 +23,7 @@ func main() {
 
 	app := fiber.New()
 
-	app.Use(func(c *fiber.Ctx) error {
+	app.Use("/ws", func(c *fiber.Ctx) error {
 		if websocket.IsWebSocketUpgrade(c) {
 			return c.Next()
 		}
@@ -27,29 +38,33 @@ func main() {
 		wsRegister <- c
 
 		for {
-			messageType, _, err := c.ReadMessage()
+			messageType, messagePayload, err := c.ReadMessage()
 			if err != nil {
 				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 					log.Println("read error:", err)
 				}
-
 				return // Calls the deferred function, i.e. closes the connection on error
 			}
-
 			if messageType == websocket.TextMessage {
-				// Broadcast the received message
-				//log.Println(string(message))
+				fmt.Println(string(messagePayload))
+				wsBroadcast <- string(messagePayload)
+				// Do something with the message
 			} else {
 				log.Println("websocket message received of type", messageType)
 			}
 		}
 	}))
 
-	mqttClient := mqttConnect()
-	go mqttListen(mqttClient, "test", subscriptionHandler)
-	go mqttListen(mqttClient, "$connected", connectionHandler)
-	go mqttListen(mqttClient, "pingcheck", pingHandler)
+	app.Use(cors.New())
+	app.Get("/init", func(c *fiber.Ctx) error {
+		return c.JSON(sensors)
+	})
 
+	mqttClient = mqttConnect()
+	go mqttListen(mqttClient, "$connected", connectionHandler)
+	go mqttListen(mqttClient, "test", broadcastHandler)
+
+	//go mqttListen(mqttClient, "pingcheck", pingHandler)
 	//token := mqttClient.Publish("pingtest", 1, false, ".")
 	//log.Println("start", time.Now().UnixNano())
 	//token.Wait()
--- mqtt.go
+++ mqtt.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -27,4 +28,58 @@ func mqttConnect() mqtt.Client {
 
 func mqttListen(client mqtt.Client, topic string, handler func(mqtt.Client, mqtt.Message)) {
 	client.Subscribe(topic, 0, handler)
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	log.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	log.Printf("Connect lost: %v", err)
+}
+
+func broadcastHandler(_ mqtt.Client, msg mqtt.Message) {
+	wsBroadcast <- "~" + msg.Topic() + ";" + string(msg.Payload())
+}
+
+func pingHandler(_ mqtt.Client, msg mqtt.Message) {
+	wsBroadcast <- string(msg.Payload())
+
+	log.Println("end  ", time.Now().UnixNano())
+	log.Println(string(msg.Payload()))
+}
+
+func connectionHandler(_ mqtt.Client, msg mqtt.Message) {
+
+	messageString := string(msg.Payload())
+
+	messageParts := strings.Split(messageString, ":")
+
+	var sensor Sensor
+
+	if messageParts[0][0:1] == "+" {
+		if len(messageParts) == 4 {
+			sensorAddress := SensorAddress(messageParts[2])
+			sensor = Sensor{
+				Hostname: SensorHostname(messageParts[1]),
+				Model:    SensorModel(messageParts[3]),
+			}
+
+			sensors[sensorAddress] = sensor
+
+			log.Println(string(sensorAddress))
+			go mqttListen(mqttClient, string(sensorAddress), broadcastHandler)
+		}
+	} else if messageParts[0][0:1] == "-" {
+		sensorAddress := SensorAddress(messageParts[2])
+		_, ok := sensors[sensorAddress]
+		if ok {
+			delete(sensors, sensorAddress)
+		}
+	}
+
+	log.Println(sensors)
+
+	wsBroadcast <- messageString
+	log.Println(messageString)
 }
